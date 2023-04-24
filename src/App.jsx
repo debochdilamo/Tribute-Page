a3dc07872b62eac0ce9aca798719094f8dd69123
dilamo deboch <123238373+debochdilamo@users.noreply.github.com>
2023-04-24 16:09:56 -0700

    instruction 5 and 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-      <main id='main'>
-        <h1 id='title'>Dr. Norman Borlaug</h1>
-        <div id="img-div">
-          <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute-page-main-image.jpg" alt="" id="image" />
-        </div>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<main id="main">
+				<h1 id="title">Dr. Norman Borlaug</h1>
+				<figure id="img-div">
+					<img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute-page-main-image.jpg" alt="" id="image" />
+					<figcaption id="img-caption">Dr. Norman Borlaug, third from the left, trains biologists in Mexico on how to increase wheat yields - part of his life-long war on hunger.</figcaption>
+				</figure>
+				<section id="tribute-info">
+          <h1>Here's a time line of Dr. Borlaug's life:</h1>
+        </section>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
